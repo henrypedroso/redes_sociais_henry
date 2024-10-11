d85abcbeae0c7064dbcb46a1b1069e87d8321943
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-10-11 09:33:29 -0300

    sssssssssssss

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,3 +1,9 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
 
-console.log(url);
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = res.json()
+    console.log(dados);
+}
+
+vizualizarInformacoesGlobais();
