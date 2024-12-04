0a7c4efe99211291be593b3086584ae5dab1c044
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-12-04 18:06:39 -0300

    sssssssssssssssssssssssssssssss

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -11,7 +11,7 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-container__texto');
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas}horas </span> e ${minutos}conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas </span> e ${minutos}conectadas.`
     
 
     const container = document.getElementById('graficos-container');
