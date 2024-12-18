ab14699365c52158205c848e9b58480cb533bad8
Henry Gasparotto <126111750+henrypedroso@users.noreply.github.com>
2024-12-18 08:40:59 -0300

    sssssssssssssssssssss

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -16,7 +16,7 @@ async function quantidadeUsuarios() {
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico)
+    Plotly.newPlot(grafico, data)
 
    
     
